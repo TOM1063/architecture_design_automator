66cb8391d727f6d7891dd1b6201672c66ac53d81
TOM1063 <tomshina.dev@gmail.com>
2023-04-20 20:16:40 +0900

    separate main func

--- text_counter/text_counter.py
+++ text_counter/text_counter.py
@@ -5,12 +5,57 @@ import openpyxl
 import pandas as pd
 
 
+
+#-------------------------------------------main-------------------------------------
+
+def main() :
+
+    # keywords = ["データ", "ビジュアライズ", "ビジュアライ","プログラミング"]
+    excel_file_name = 'data/result.xlsx'
+
+    sheet_df = pd.read_excel(excel_file_name,sheet_name="input")
+    print(sheet_df)
+    keywords_df = sheet_df.iloc[0:,0]
+    keywords = keywords_df.to_numpy().tolist()
+    print(keywords)
+
+
+    url_sheet_df = pd.read_excel(excel_file_name,sheet_name="url")
+    print(url_sheet_df)
+    powerpoint_file_name = str(url_sheet_df.iat[0,0])
+    print("power_point_file : " + powerpoint_file_name)
+
+
+    prs = Presentation(powerpoint_file_name)
+    db = get_text_data(prs)
+    count_result = counter(db,keywords)
+    count_result_df = pd.DataFrame(count_result).transpose()
+    count_result_df = pd.concat([count_result_df,pd.DataFrame(count_result_df.sum(axis=1),columns=['Total'])],axis=1)
+    # print(count_result_df)
+
+    update_sheet_df = pd.concat([sheet_df, count_result_df], axis=1)
+    print(update_sheet_df)
+
+    # with pd.ExcelWriter(excel_file_name, mode='a') as writer:
+    #     update_sheet_df.to_excel(writer, sheet_name ='result')
+
+    with pd.ExcelWriter(excel_file_name) as writer:
+        sheet_df.to_excel(writer, sheet_name ='input',index=False)
+        url_sheet_df.to_excel(writer, sheet_name ='url',index=False)
+        update_sheet_df.to_excel(writer, sheet_name ='result',index=False)
+#-------------------------------------------main-------------------------------------
+
+
+
+
+#-----------------------------------------function-----------------------------------
+
 # get paragraph array by each slide
 def get_text_data(presentation):
     slide_num = len(presentation.slides)
     text_db = []
     for i in range(slide_num):
-        slide = prs.slides[i]
+        slide = presentation.slides[i]
         # print(slide)
         texts = []
         for shape in slide.shapes:
@@ -42,41 +87,8 @@ def counter(database,keywords_list):
 
     #print(result)
     return result
+#-----------------------------------------function-----------------------------------
 
 
-
-#-----------------------------------------main-----------------------------------
-
-# keywords = ["データ", "ビジュアライズ", "ビジュアライ","プログラミング"]
-excel_file_name = 'data/result.xlsx'
-
-sheet_df = pd.read_excel(excel_file_name,sheet_name="input")
-print(sheet_df)
-keywords_df = sheet_df.iloc[0:,0]
-keywords = keywords_df.to_numpy().tolist()
-print(keywords)
-
-
-url_sheet_df = pd.read_excel(excel_file_name,sheet_name="url")
-print(url_sheet_df)
-powerpoint_file_name = str(url_sheet_df.iat[0,0])
-print("power_point_file : " + powerpoint_file_name)
-
-
-prs = Presentation(powerpoint_file_name)
-db = get_text_data(prs)
-count_result = counter(db,keywords)
-count_result_df = pd.DataFrame(count_result).transpose()
-count_result_df = pd.concat([count_result_df,pd.DataFrame(count_result_df.sum(axis=1),columns=['Total'])],axis=1)
-# print(count_result_df)
-
-update_sheet_df = pd.concat([sheet_df, count_result_df], axis=1)
-print(update_sheet_df)
-
-# with pd.ExcelWriter(excel_file_name, mode='a') as writer:
-#     update_sheet_df.to_excel(writer, sheet_name ='result')
-
-with pd.ExcelWriter(excel_file_name) as writer:
-    sheet_df.to_excel(writer, sheet_name ='input',index=False)
-    url_sheet_df.to_excel(writer, sheet_name ='url',index=False)
-    update_sheet_df.to_excel(writer, sheet_name ='result',index=False)
+if __name__ == "__main__":
+    main()
